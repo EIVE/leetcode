e7e9fbf5a34dc5a0151d5734b2a44bc0b7d247ed
EIVE <1074418022@qq.com>
2019-06-13 07:53:18 +0800

    binary-tree-level-order-traversal-ii

---
+++ easy/107.binary-tree-level-order-traversal-ii.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=107 lang=javascript
+ *
+ * [107] Binary Tree Level Order Traversal II
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrderBottom = function(root) {
+    if (!root) return [];
+    var array = [];
+    search(root, 1);
+
+    function search(root, level) {
+        if(!root) return;
+        if (array.length < level) {
+            array.unshift([]);
+        }
+        var arr = array[array.length - level];
+        arr.push(root.val);
+        search(root.left, level + 1);
+        search(root.right, level + 1);
+    }
+
+    return array;
+};
+
