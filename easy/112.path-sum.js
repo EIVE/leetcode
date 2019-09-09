6946018404e9d0bcf782a9d722e88ca96166c6a6
chirs.zeng <chirs.zeng@gllue.com>
2019-09-09 15:53:53 +0800

    path-sum

---
+++ easy/112.path-sum.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=112 lang=javascript
+ *
+ * [112] Path Sum
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, sum) {
+    if (!root) {
+        return false;
+    }
+    if (!root.left && !root.right && root.val === sum) {
+        return true;
+    }
+    return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val)
+};
+
