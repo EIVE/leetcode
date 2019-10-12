2845a335a23903c7593150a073305a0595af4ab0
EIVE <1074418022@qq.com>
2019-10-12 10:31:17 +0800

    移除链表元素

---
+++ easy/203.移除链表元素.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=203 lang=javascript
+ *
+ * [203] 移除链表元素
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+    if (!head) return null;
+    const res = removeElements(head.next, val);
+    if (head.val === val) return res;
+    head.next = res;
+    return head;
+};
+// @lc code=end
+
